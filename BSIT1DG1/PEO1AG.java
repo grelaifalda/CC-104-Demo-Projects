f9a7c6d9033fd025ebe7157e6022b661da395d13
grelaifalda <laigrefalda@gmail.com>
2026-04-19 08:31:31 +0800

    PEO1AG.java

---
+++ BSIT1DG1/PEO1AG.java
@@ -0,0 +1,31 @@
+/* 
+
+Grefalda, Alyssa Sofia D.
+BSIT1DG1
+This will determine if odd or even
+*/
+
+import java.util.*;
+class PE01AG {
+
+public static void main (String Lai []){
+
+  Scanner ent=new Scanner (System.in);
+	int jn=0;
+
+	System.out.print("Enter a Number: ");	
+	jn=ent.nextInt();
+
+	if (jn % 2 != 0)
+	{
+	System.out.print("The number is Odd");	
+	}
+	else
+	{
+	System.out.print("The number is Even");	
+	}
+	
+
+	}
+
+}
