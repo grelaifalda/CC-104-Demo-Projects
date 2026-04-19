d901767b13704b1fc19e0f55ed2347735e34fbbf
grelaifalda <laigrefalda@gmail.com>
2026-04-19 08:26:45 +0800

    MathOper.java

---
+++ BSIT1DG1/MathOper.java
@@ -0,0 +1,19 @@
+class MathOper {
+
+    public static void main (String Lai []){
+        int N1=10, N2=4;
+        int Sum,Diff,Prod,Quot,Rem;
+
+    Sum=N1+N2;
+    System.out.println("Sum = " + Sum);
+    Diff = N1 - N2;   
+    System.out.println("Difference = " + Diff);
+    Prod = N1 * N2;   
+    System.out.println("Product = " + Prod);
+    Quot = N1 / N2;   
+    System.out.println("Quotient = " + Quot);
+    Rem = N1 % N2;   
+    System.out.println("Remainder = " + Rem);
+}
+
+}
